7c2f928224bf3560a6d1ae62633dca3e5e55e735
Rossi-Davide <davide0812rossi@gmail.com>
2021-11-12 08:45:54 +0100

    Realizzazione proprietà biblioteca

---
+++ Software Biblioteca/Biblioteca.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software_Biblioteca
+{
+    class Biblioteca
+    {
+
+        private string _nome;
+
+        public string Nome
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Il nome non può essere vuoto");
+                }
+
+                _nome = value;
+            }
+
+            get
+            {
+                return _nome;
+            }
+        }
+
+        private string _indirizzo;
+
+        public string Indirizzo
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("L'indirizzo non può essere vuoto");
+                }
+
+                _indirizzo = value;
+            }
+
+            get
+            {
+                return _indirizzo;
+            }
+        }
+
+        public List<DateTime> OrariApuertura { set; get; }
+
+        public List<DateTime> OrariChisura { set; get; }
+
+        public List<Libro> Libri { set; get; }
+
+
+    }
+}
